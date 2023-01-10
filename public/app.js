8ca6865bd5587945d66e796e1a5db429a77a6034
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-01-10 23:57:06 +0100

    Classes

--- public/app.js
+++ public/app.js
@@ -1,5 +1,24 @@
 "use strict";
+//classes
+class Invoice {
+    client;
+    details;
+    amount;
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} χρωστάει ${this.amount} για το/τα ${this.details}}`;
+    }
+}
+const invOne = new Invoice('Μητσουέλ', 'Κοψίδια', 35);
+const invTwo = new Invoice('Χωστούδς', 'Πίτσες', 30);
+const invThree = new Invoice('Ζορζάκ', 'Πιτόγυρα', 25);
+let invoices = [];
 const form = document.querySelector('.new-item-form');
+// inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,29 @@
+//classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} χρωστάει ${this.amount} για το/τα ${this.details}}`
+  }
+}
+
+const invOne = new Invoice('Μητσουέλ', 'Κοψίδια', 35);
+const invTwo = new Invoice('Χωστούδς', 'Πίτσες', 30);
+const invThree = new Invoice('Ζορζάκ', 'Πιτόγυρα', 25);
+
+let invoices: Invoice[] = [];
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
+// inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
